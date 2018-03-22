facaf40b34d79a865c584033a1aea0276f13f26e
staryjie <staryjie@163.com>
2018-03-22 13:03:19 +0800

    variable

---
+++ variable/c9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# val = 9 / 2
+# print(val)
+
+# s = "abcdefg"
+# print("bc" in s)
+# li = ['alex','eric','lily']
+# print('alex' in li)
---
+++ variable/variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# name = input("请输入姓名： ")
+# print("欢迎你，",name)
+
+tmp = "hey"
+print(tmp)
+tmp_new = tmp.upper()
+print(tmp_new)
+print(tmp.title())
