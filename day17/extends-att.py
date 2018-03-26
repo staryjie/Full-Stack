eef6f1f6aaeb7e6931c8c376f5bc13539681fa21
staryjie <staryjie@163.com>
2018-03-26 15:15:07 +0800

    面向对象-super执行父类构造方法

--- day17/extends-att.py
+++ day17/extends-att.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python3
 # -*- coding:utf-8 -*-
 
+
+'''
 # 多继承易错点
 
 class A:
@@ -31,3 +33,23 @@ e1.bar()
 
 b1 = B()
 b1.bar()
+'''
+
+
+
+class Animal:
+    def __init__(self):
+        print("Animal的构造方法")
+        self.ty = "动物"
+
+class Cat(Animal):
+    def __init__(self):
+        print("Cat的构造方法")
+        self.n = "猫"
+        # self.ty = "动物"  # 执行父类的构造方法
+        super(Cat,self).__init__() # 执行父类的构造方法
+        # Animal.__init__(self)  # 执行父类的构造方法
+
+# a = Animal()
+c = Cat()
+print(c.__dict__)
