adc9df169141e1d34cd7d8b82b4268f2e985e4a5
staryjie <staryjie@163.com>
2018-03-26 14:21:04 +0800

    面向对象-断言

---
+++ day16-abnormal/assertion.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# -*- coding:utf-8 -*-
+
+# 断言
+
+assert 1 == 2
+
+'''
+try:
+    if 1 == 2:
+        pass
+    else:
+        raise Exception("hehe")
+except Exception as e:
+    print(e)
+'''
