686fba3d134df7f690be6df2f14c80d8dedf8480
staryjie <staryjie@163.com>
2018-03-26 11:33:32 +0800

    面向对象-类的创建和其实现

--- day16/special-iter.py
+++ day16/special-iter.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3
 # -*- coding:utf-8 -*-
 
-# 特殊类成员 - iter
+# 特殊类成员 - iter\__mataclass__\__new__
 
 class Foo:
+
+    __mataclass__ = type #
 
     def __init__(self):
         pass
@@ -18,3 +20,37 @@ obj = Foo()
 
 for i in obj: # 循环对象的时候，默认就去循环类中__iter__生成的数据，就是去循环生成器
     print(i)
+
+print(type(obj))
+print(type(Foo))
+# <class 'type'>
+# Python中一切皆对象，类也属于对象，类默认是由type创建的
+
+
+# 类可以创建对象，类默认是由type创建的，也可以由__mataclass__ = xxx 指定由xxx来创建
+
+# 写个例子：
+
+class MyType(type):
+
+    def __init__(self,what,bases=None,dict=None):
+        super(MyType,self).__init__(what,bases,dict)
+
+    def __call__(self, *args, **kwargs):
+        obj = self.__new__(self,*args,**kwargs)
+
+        self.__init__(obj)
+
+class Foo(object):              # Foo = MyType(...)
+    __mataclass__ = MyType      # 指定Foo类由MyType创建
+
+    def __init__(self,name):
+        self.name = name
+
+    def __new__(cls, *args, **kwargs):
+        return object.__new__(cls,*args,**kwargs)
+
+# 第一阶段：解释器从上到下执行代码创建Foo类
+# 第二阶段：通过Foo类创建obj对象
+
+obj = Foo()
