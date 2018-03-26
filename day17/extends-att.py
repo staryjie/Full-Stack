db24b09c42ce3581708e27476d9bb77967dad861
staryjie <staryjie@163.com>
2018-03-26 15:01:05 +0800

    面向对象-多继承易错点

---
+++ day17/extends-att.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+# -*- coding:utf-8 -*-
+
+# 多继承易错点
+
+class A:
+    def bar(self):
+        self.f1()
+
+class B(A):
+    def f1(self):
+        print("B")
+
+class C:
+    def f1(self):
+        print("C")
+
+class D(C,B):
+    pass
+
+class E(B,C):
+    pass
+
+d1 = D()
+d1.f1()
+d1.bar()
+
+e1 = E()
+e1.f1()
+e1.bar()
